d8bdcab5133e398a4290d7390d7b174d4b4f863c
Alan Antony <voa.alan@gmail.com>
2025-10-06 07:09:49 +0530

    cors updated

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,11 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173", // Vite default port
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://cini-grid-1nyv.vercel.app",
+    ],
     credentials: true,
   })
 );
