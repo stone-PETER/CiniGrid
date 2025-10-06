0d3355263964ab929831a75ceff894d373cef590
Alan Antony <voa.alan@gmail.com>
2025-10-06 07:11:58 +0530

    cors new link added

--- backend/index.js
+++ backend/index.js
@@ -31,6 +31,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:3000",
       "https://cini-grid-1nyv.vercel.app",
+      "https://cini-grid.vercel.app/",
     ],
     credentials: true,
   })
